48c79e1431764ee334d70f780c2470dd4d29815a
illonaaddae <iamillona23@gmail.com>
2024-09-21 11:33:02 +0000

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '633b5e3b-df89-448f-9154-92bf5bb04488';
+export const MODAL_CLOSE_SEC = 2.5;
